d1d400a2e5fb4097b3ad805a93e6f845e8bdeae5
Ingyin Khine <ingyin.khine.fb.2020.2021@gmail.com>
2024-07-06 23:04:48 +0630

    [Complete]  LatHtaukBayDin

---
+++ IGK.DotNetTrainingBatch4.RestApiWithNLayer/Features/LatHtaukBayDin/LatHtaukBayDinController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace IGK.DotNetTrainingBatch4.RestApiWithNLayer.Features.LatHtaukBayDin;
+
+[Route("api/[controller]")]
+public class LatHtaukBayDinController : Controller
+{
+    private async Task<LatHtaukBayDin> GetDataAsync()
+    {
+        string jsonString = await System.IO.File.ReadAllTextAsync("data.json");
+        var model = JsonConvert.DeserializeObject<LatHtaukBayDin>(jsonString);
+        return model;
+    }
+
+    [HttpGet("questions")]
+    public async Task<IActionResult> Questions()
+    {
+        var model = await GetDataAsync();
+        return Ok(model.questions);
+    }
+
+    [HttpGet("numberlist")]
+    public async Task<IActionResult> NumberList()
+    {
+        var model = await GetDataAsync();
+        return Ok(model.numberList);
+    }
+
+    [HttpGet("{questionNo}/{no}")]
+    public async Task<IActionResult> QuestionAnswer(int questionNo, int no)
+    {
+        var model = await GetDataAsync();
+        return Ok(model.answers.FirstOrDefault(x => x.questionNo == questionNo && x.answerNo == no));
+    }
+
+    public class LatHtaukBayDin
+    {
+        public List<Question> questions { get; set; }
+        public List<Answer> answers { get; set; }
+        public List<string> numberList { get; set; }
+    }
+
+    public class Question
+    {
+        public int questionNo { get; set; }
+        public string questionName { get; set; }
+    }
+
+    public class Answer
+    {
+        public int questionNo { get; set; }
+        public int answerNo { get; set; }
+        public string answerResult { get; set; }
+    }
+}
---
+++ IGKDotNetTrainingBatch4ConsoleAppHTTPClient/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+Console.WriteLine("Hello, World!");
+
+String jsonStr = await File.ReadAllTextAsync("data.json");
+var model = JsonConvert.DeserializeObject<MainDTO>(jsonStr);
+
+// Console.WriteLine(jsonStr);
+
+//JSON to C#?? need package 
+// C# to JSON
+
+foreach (var queation in model.questions)
+{
+    Console.WriteLine(queation.questionName);
+}
+
+Console.ReadLine();
+
+static string ToNumber(string num)
+{
+    num = num.Replace("၃", "3");
+    num = num.Replace("၃", "3");
+    num = num.Replace("၃", "3");
+    num = num.Replace("၃", "3");
+    return num;
+}
+
+public class MainDTO
+{
+    public Question[] questions { get; set; }
+    public Answer[] answers { get; set; }
+    public string[] numberList { get; set; }
+}
+
+public class Question
+{
+    public int questionNo { get; set; }
+    public string questionName { get; set; }
+}
+
+public class Answer
+{
+    public int questionNo { get; set; }
+    public int answerNo { get; set; }
+    public string answerResult { get; set; }
+}
+
